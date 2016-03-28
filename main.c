7d07681e9b1a1dda841df82360199ee2bfc024f7
alexlevine66 <alexlevine@email.arizona.edu>
2016-03-27 21:34:53 -0700

    Added files via upload

--- main.c
+++ main.c
@@ -16,107 +16,96 @@
 #include "switch.h"
 #include "keypad.h"
 #include "adc.h"
+#include "pwm.h"
 #include <stdlib.h>
 
 
 #define CLR 0x01
 
-#define RUN_LED LATDbits.LATD2
-#define STP_LED LATDbits.LATD0
-#define RUN TRISDbits.TRISD2
-#define STP TRISDbits.TRISD0
+#define PINTypeIN1 TRISDbits.TRISD10 //J11 Pin 20, RD1
+#define PINTypeIN2 TRISDbits.TRISD11 //J11 Pin 14, RC13
 
-#define SWT_T TRISDbits.TRISD5
-#define SWT PORTDbits.RD5
+#define PINTypeIN3 TRISDbits.TRISD7 // J11 Pin 18, RD3
+#define PINTypeIN4 TRISDbits.TRISD8 // J10 Pin 15, RD11
 
-#define RST_T TRISDbits.TRISD6
-#define RST PORTDbits.RD6
+#define PINTypeSW TRISDbits.TRISD6
 
-#define ROW_1 LATDbits.LATD12
-#define ROW_2 LATDbits.LATD6
-#define ROW_3 LATDbits.LATD5
-#define ROW_4 LATDbits.LATD11
+#define INPUT1 LATDbits.LATD10 // input 1
+#define INPUT2 LATDbits.LATD11  // input 2
+#define INPUT3 LATDbits.LATD7  // input 3
+#define INPUT4 LATDbits.LATD8  // input 4
 
-#define COL_1 PORTGbits.RG0
-#define COL_2 PORTGbits.RG13
-#define COL_3 PORTGbits.RG12
+#define INPUT 1
+#define OUTPUT 0
+
+#define SEND 1
+#define OFF 0
+
 
 
 // ******************************************************************************************* //
 
 typedef enum RS_enum {
-    wait, check, write, changeM1, changeM2
-}status;
+    wait, idle, wait2, forward, wait3, backward, wait4, idle2, 
+}stateType;
 
-volatile int q = 0;
-volatile int d = 0;
-volatile int line = 1;
 volatile unsigned int val;
-volatile unsigned int digitalValue = 0;
-volatile char k = -1;
-volatile int x = 0;
-volatile char password[5] = {' ', ' ', ' ', ' ', '\0'};
-volatile int place = 0;
 
-volatile char setCheck[5] = {'*', '*', ' ', ' ', '\0'};
-volatile char invalid1 = '*';
-volatile char invalid2 = '#';
-
-volatile status state = wait;
-volatile status next;
+//volatile status state = wait;
+//volatile status next;
 
 int main(void)
-{
-    enableInterrupts();
-    
-    
+{   
     SYSTEMConfigPerformance(10000000);
-    STP = 0;
-    RUN = 0;
+    enableInterrupts();
     double analog=0;
     int i;
     
-    LATDbits.LATD0 = 1;
+    //PINTypeSW=1;
+    PINTypeIN1=OUTPUT;
+    PINTypeIN2=OUTPUT;
+    PINTypeIN3=OUTPUT;
+    PINTypeIN4=OUTPUT;
+    
+    INPUT1=OFF;
+    INPUT2=OFF;
+    INPUT3=OFF;
+    INPUT4=OFF;
+    
     initTimer2();
     initLCD();
     initADC();
-    delayMs(1000);
-    LATDbits.LATD0 = 0;
-    val=0;
-    TRISDbits.TRISD0 = 0;
+    initPWM();
+    
     int radix=10;
     writeCMD(CLR);
-    //clearLCD();
+    
     moveCursorLCD(0,2);
-    testLCD();
+    //testLCD();
     char buf[5];
     const char* string;
-    //string="00000";
     
-    //delayMs(1000);
-    //printStringLCD("In Loop");        
-    while(1)
-    {       
+    
+    while(1){
+        
         clearLCD();
         if(IFS0bits.AD1IF == 1)
         {
             IFS0bits.AD1IF = 0;
             val=ADC1BUF0;
-            printStringLCD("Voltage= ");
-           // delayMs(500);
+            //OC2RS=val+500;
+            //OC4RS=val+500;
             analog=(3.3*val)/1023;
-            //itoa(buf,analog,radix);
             sprintf(buf, "%1.1f", analog);
             string=buf;
             printStringLCD(string);
-            val=0;
-            delayMs(100);
+            delayMs(10);
         }
         
-        for(i = 0; i < (int) (1000*val)/1023; i++) delayUs(100);
-        LATDbits.LATD0 = 1;
-        for(i = 0; i < (int) (1000*(1023 - val))/1023; i++) delayUs(100);
-        LATDbits.LATD0 = 0;
+        OC4RS=800;
+        OC5RS=1000;
+        
+        
     }
     return 0;
 }
@@ -125,3 +114,4 @@ int main(void)
   //  IFS0bits.AD1IF = 0;
   //  val = ADC1BUF0;
 //}
+
--- pwm.c
+++ pwm.c
@@ -1,12 +1,37 @@
 
 #include <xc.h>
+//#include <proc/p32x470f5121.h>
+#include "pwm.h"
 
 
 void initPWM(){
-    RPD1Rbits.RPD1R = 0b1011; // map OC2 to RD1
-    OC2CON = 0x0000; // Turn off OC1 while doing setup.
-    OC2R = 0x0000; // Initialize primary Compare Register
-    OC2RS = 0x0000; // Initialize secondary Compare Register
-    OC2CON = 0x0006; // Configure for PWM mode
-    OC2CONSET = 0x8000; // Enable OC1
+    RPD1Rbits.RPD1R = 0; // map OC2 to RD1, input 1
+    RPD9Rbits.RPD9R = 0b1011;    //input 2
+    RPD3Rbits.RPD3R = 0; // map OC4 to RD3, input 3 
+    
+    RPD11Rbits.RPD11R = 0b1011;   //input 4
+    T3CONbits.TCKPS=0;
+    PR3=1023;
+    TMR3=0;
+    T3CONbits.TON=1;
+    OC2CONbits.OCTSEL=1;
+    OC2CONbits.OCM=6;       // Configure for PWM mode
+    OC2RS=0;
+    OC2R=0;     // Initialize primary Compare Register
+    
+    OC4CONbits.OCTSEL=1;
+    OC4CONbits.OCM=6;       // Configure for PWM mode
+    OC4RS=0;
+    OC4R=0;     // Initialize primary Compare Register
+    
+    OC5CONbits.OCTSEL=1;
+    OC5CONbits.OCM=6;       // Configure for PWM mode
+    OC5RS=0;
+    OC5R=0;     // Initialize primary Compare Register
+    
+    OC2CONbits.ON=1;
+    OC4CONbits.ON=1;
+    OC5CONbits.ON=1;
+    
+    
 }
